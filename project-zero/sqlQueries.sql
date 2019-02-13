79a15c7835774777502f71a2c570c4136a1c28cd
JTGibbons1993 <JTGibbons1993@gmail.com>
2019-02-13 08:26:31 -0500

    added sql queries

---
+++ project-zero/sqlQueries.sql
@@ -0,0 +1,11 @@
+    
+CREATE TABLE LockedUser(
+    accountL int,
+    lockedName varchar2(50), 
+    lockedpassword varchar2(50), 
+    lockedssn int, 
+    lockedtype varchar2(50));
+    
+CREATE TABLE ACCOUNTA(
+    accountNumber int, 
+    accountBalance int);
